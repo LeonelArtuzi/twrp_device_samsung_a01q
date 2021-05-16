89bbeafcaac6218e002caa1f082b3998dc1e7774
Leonel Artuzi <leonel.artu@gmail.com>
2021-05-16 10:55:54 -0300

    Fix Dynamic Partitions

--- omni_a01q.mk
+++ omni_a01q.mk
@@ -18,19 +18,23 @@ $(call inherit-product, vendor/omni/config/gsm.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit language packages
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/a01q/device.mk)
 
 PRODUCT_PACKAGES += \
     charger_res_images \
     charger
 
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/recovery/root/etc/recovery.fstab:recovery.fstab \
-	system/timezone/output_data/iana/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
-
-#	$(LOCAL_PATH)/kernel:kernel \
-
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/a01q/recovery/root,recovery/root)
 
 PRODUCT_DEVICE	:= a01q
 PRODUCT_NAME	:= omni_a01q
@@ -40,9 +44,9 @@ PRODUCT_MANUFACTURER := Samsung
 
 #BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := false
 
-PRODUCT_PROPERTY_OVERRIDES := ro.treble.enabled=true
+#PRODUCT_PROPERTY_OVERRIDES := ro.treble.enabled=true
 
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.build.version.security_patch=2099-12-31 \
-	ro.vendor.build.security_patch=2099-12-31 \
-	ro.product.cpu.abilist=arm64-v8a,armeabi-v7a,armeabi
+#PRODUCT_PROPERTY_OVERRIDES += \
+#	ro.build.version.security_patch=2099-12-31 \
+#	ro.vendor.build.security_patch=2099-12-31 \
+#	ro.product.cpu.abilist=arm64-v8a,armeabi-v7a,armeabi
